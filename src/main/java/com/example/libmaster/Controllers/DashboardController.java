3e2e4aca821a02311b4afb88292d44084dd2baad
nq7864 <nq7864@users.noreply.github.com>
2025-04-26 11:00:40 +0700

    Add 'Dashboard Controller'

--- src/main/java/com/example/libmaster/Controllers/DashboardController.java
+++ src/main/java/com/example/libmaster/Controllers/DashboardController.java
@@ -1,4 +1,16 @@
 package com.example.libmaster.Controllers;
 
+import com.example.libmaster.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class DashboardController {
+    @FXML
+    private void handleAddBookChoiceBtn(MouseEvent event) throws IOException {
+        Main.changeScene("addBookChoice.fxml");
+        System.out.println("Button Clicked!");
+    }
 }
