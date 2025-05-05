ff1c5edf363355ae3f77095a931df3eb931b2f24
anoreo0207 <pejij2004@gmail.com>
2025-05-05 10:14:45 +0700

    Add 'Member Info' Controller

---
+++ src/main/java/com/example/libmaster/Controllers/MemberInfoController.java
@@ -0,0 +1,24 @@
+package com.example.libmaster.Controllers;
+
+import com.example.libmaster.Models.Person.Member;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MemberInfoController {
+    @FXML private TextField idField;
+    @FXML private Label nameLabel;
+    @FXML private TextField dobField;
+    @FXML private Label genderLabel;
+    @FXML private TextField phoneField;
+    @FXML private TextField emailField;
+
+    public void setMember(Member member) {
+        idField.setText(String.valueOf(member.getId()));
+        nameLabel.setText(member.getName());
+        dobField.setText(member.getDateOfBirth());
+        genderLabel.setText(member.getGender());
+        phoneField.setText(member.getPhone());
+//        emailField.setText(member.getEmail());
+    }
+}
