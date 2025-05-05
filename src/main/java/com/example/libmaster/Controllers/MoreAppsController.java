5322d559fcbfed555b0591b68a1d5469b8d225c3
anoreo0207 <pejij2004@gmail.com>
2025-05-05 10:13:39 +0700

    Add 'More Apps' Controller

---
+++ src/main/java/com/example/libmaster/Controllers/MoreAppsController.java
@@ -0,0 +1,27 @@
+package com.example.libmaster.Controllers;
+
+import javafx.fxml.FXML;
+
+public class MoreAppsController {
+
+    private void handleLaunchApp() {
+
+    }
+
+    @FXML
+    private void visitAppOne() {
+
+    }
+
+    @FXML
+    private void visitAppTwo() {
+
+    }
+
+    @FXML
+    private void visitAppThree() {
+
+    }
+
+
+}
